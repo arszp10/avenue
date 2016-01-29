43d401c210baf2da1ef11c8974e1bfcf41102f50
Mikhail Kuzin <mikhail@textmagic.biz>
2016-01-29 11:44:20 +0700

    stopline intervals

--- stop-line.js
+++ stop-line.js
@@ -2,84 +2,121 @@
 function stopLine(options)
 {
     var defaults = {
-        hash: 5454651340015154,
-        avgIntensity: 600,
-        avgIntensityPerSecond: 600/3600,
-        capacity: 1800,
-        capacityPerSecond: 1800/3600,
-        cicleTime: 60,
-        redTime: [0,1,2,3,4,5,6,7,8,9]
+        hash: 0000,
+        avgIntensity: 1800,
+        avgIntensityPerSecond: 1800/3600,
+        capacity: 3600,
+        capacityPerSecond: 3600/3600,
+        cicleTime: 100,
+        intervals: [
+            {s:0, f:9, length: 10},
+            {s:60, f:67, length: 8},
+            {s:90, f:99, length: 10}
+        ],
     };
 
     this.options = defaults;
 }
 
 
-/**
- *
- * @param inFlow
- * @returns {number}
- */
-stopLine.prototype.getLastRedPoint = function(){
-    if (this.options.redTime.length == 0) {
-        return 0;
-    }
-    var i = this.options.cicleTime - this.options.redTime[this.options.redTime.length-1];
-    while (this.options.redTime.indexOf(this.options.cicleTime - i) > -1) {
-        i++;
-    }
-    return this.options.cicleTime - i + 1;
-
-}
-
-stopLine.prototype.calc = function(inFlow)
+stopLine.prototype.calc = function(inFlow, queueTail)
 {
+    var cicleTime = this.options.cicleTime;
+    var capacityPerSecond = this.options.capacityPerSecond;
     if (inFlow == undefined) {
-        inFlow = arrayFill(this.options.cicleTime, this.options.avgIntensityPerSecond);
+        inFlow = [].fill(cicleTime, this.options.avgIntensityPerSecond);
     }
-    var outFlow = arrayFill(this.options.cicleTime, 0);
+    var outFlow = [].fill(cicleTime, 0);
     var delay = 0;
-    var queue = 0;
-    var start = this.getLastRedPoint();
+    var queue = queueTail == undefined ? 0 : queueTail;
+
+    var intervals = this.options.intervals;
+    var last = intervals.last();
+    var first = intervals.first();
+
+    if (last && first) {
+        if((last.f + 1) % cicleTime == first.s) {
+            last.length += first.length;
+        }
+    }
+
+    var start = last.s;
+    var currIntervalInx = intervals.length - 1;
+    var currInterval = intervals[currIntervalInx];
+    var rTime = currInterval.length;
     var t = 0;
-    var r = 10;
-    inFlow.map(function(v, i, data){
-        var j = (i + start) % this.options.cicleTime;
-        var value = data[j];
+    var sumInFlow = 0;
+    var sumOutFlow = 0;
+
+    for (var i = 0; i < inFlow.length; i++){
+        var j = (i + start) % cicleTime;
+        var value = inFlow[j];
+        sumInFlow += value;
         queue += value;
-        if (this.options.redTime.indexOf(j) > -1) {
-            delay += (r - t + Math.floor(queue/this.options.capacityPerSecond))*value;
-            //console.log("---", t, r-t+ Math.floor(queue/this.options.capacityPerSecond),value);
+        if (j >= currInterval.s && j<= currInterval.f) {
+            delay += (rTime - t + Math.floor(queue/capacityPerSecond))*value;
             t++;
             outFlow[j] = 0;
-            return 0;
-        } else if (queue <= this.options.capacityPerSecond){
+            //console.log('OUT: ', j, outFlow[j], queue ,rTime - t);
+            if (j != currInterval.f) {
+                continue;
+            }
+
+            currIntervalInx = (currIntervalInx + 1) % intervals.length;
+            currInterval = intervals[currIntervalInx];
+            rTime = currInterval.length;
+            t = 0;
+            continue;
+
+        } else if (queue <= capacityPerSecond){
             value = queue;
             queue -= value;
         } else {
-            value = this.options.capacityPerSecond;
+            value = capacityPerSecond;
             queue -= value;
-            delay += Math.floor(queue/this.options.capacityPerSecond)*value;
-            //console.log("---",Math.floor(queue/this.options.capacityPerSecond),value);
+            delay += Math.floor(queue/capacityPerSecond)*value;
         }
+
         t = 0;
         outFlow[j] = value;
-        return value;
-    }, this);
+        //console.log('OUT: ', j, outFlow[j], queue, 0);
+        sumOutFlow += value;
+        continue;
+    }
 
-    console.log(delay);
+    //console.log('QUEUE REST: ',queue);
+    //console.log('DELAY: ',delay);
     //console.log(outFlow);
-    console.log(
-        outFlow.reduce(function(a, b) { return a + b; }, 0),
-        inFlow.reduce(function(a, b) { return a + b; }, 0)
-    );
+    //console.log('SUM: ', sumInFlow, sumOutFlow);
+    if (sumInFlow != sumOutFlow) {
+        return this.calc(inFlow, queue);
+    }
+    return outFlow;
 };
 
 
-function arrayFill(len, value){
+Array.prototype.fill = function(len, value){
     return Array.apply(null, Array(len)).map(Number.prototype.valueOf,value);
-}
+};
 
+Array.prototype.last = function(){
+    if (this.length > 0){
+        return this[this.length - 1];
+    }
+    return false;
+};
+
+Array.prototype.first = function(){
+    if (this.length > 0){
+        return this[0];
+    }
+    return false;
+};
 
 var sl = new stopLine();
-sl.calc();
+console.log(new Date());
+for (var i = 0; i < 100000; i++) {
+    sl.calc();
+}
+console.log(new Date());
+
