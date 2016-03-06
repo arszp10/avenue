319c5ca75d94f0bbb97ee564546dbc8d361161b8
Кузин Михаил <kuzin_mv@inbox.ru>
2016-03-06 14:27:40 +0700

    refactoring

---
+++ frontend/js/_bundle.js
@@ -0,0 +1,25 @@
+//= vendor/socket.io.min.js
+//= vendor/jquery/jquery-2.2.1.min.js
+//= vendor/bootstrap/bootstrap.min.js
+//= vendor/cytoscape/cytoscape.min.js
+//= vendor/cytoscape/cytoscape-edgehandles.js
+//= vendor/cytoscape/cytoscape-panzoom.js
+//= vendor/cytoscape/cytoscape-navigator.js
+
+//= vendor/charts/chartist.min.js
+//= vendor/i-check/icheck.min.js
+//= vendor/utils/drag.js
+//= vendor/bootstrap/bootstrap-slider.min.js
+
+//= app/app.js
+//= app/html-templates.js
+//= app/settings.js
+//= app/network-samples.js
+//= app/cy-styles.js
+//= app/ui-events.js
+//= app/cy-events.js
+//= app/ws-client.js
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-minify-css'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html:   'frontend/*.html',
+        js:     'frontend/js/_bundle.js',
+        style:  'frontend/css/_bundle.css',
+        img:    'frontend/img/**/*.*',
+        fonts:  'frontend/fonts/**/*.*'
+    },
+    watch: {
+        html:   'frontend/**/*.html',
+        js:     'frontend/js/**/*.js',
+        style:  'frontend/css/*.css',
+        img:    'frontend/img/**/*.*',
+        fonts:  'frontend/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "avenue-Log_"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+    gulp.src('frontend/js/vendor/ace/**')
+        .pipe(gulp.dest(path.build.js + 'ace'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('css:build', function () {
+    gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        //.pipe(sass())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'css:build',
+    'fonts:build',
+    'image:build'
+]);
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('css:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+
+gulp.task('default', ['build', 'webserver', 'watch']);
