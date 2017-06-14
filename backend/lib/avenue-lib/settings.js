e01bd00ed3a7d47b82f8e94beb596c3d68676cef
Кузин Михаил <kuzin_mv@inbox.ru>
2017-06-15 01:18:38 +0700

    todo modified

--- backend/lib/avenue-lib/settings.js
+++ backend/lib/avenue-lib/settings.js
@@ -6,7 +6,7 @@ module.exports = {
         yellow: 3,
         totalLength: 6
     },
-    
+
     offsetOptimizationSteps: function(Tc) {
         var c = [0.4, 0.3, 0.2, 0.1, 0.05, 0.3, 0.2, 0.1, 0.05]; // + 2, 1
         var r = c.map(function(v){ return Math.round(Tc * v);});
